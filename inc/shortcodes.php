b14f54d98cf1fe6896cc103d2f7d62cef0a45447
Slant Dev <build@amazeworx.com>
2024-06-04 01:19:37 +0700

    featured badge shortcode

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -4,6 +4,15 @@
 function listeo_featured_badge_function($atts)
 {
   global $post;
-  echo $post->ID;
+  $is_featured = listeo_core_is_featured($post->ID);
+
+  $output = '';
+  if ($is_featured) {
+    $output .= '<div class="listing-small-badges-container">';
+    $output .= '<div class="listing-small-badge featured-badge"><i class="fa fa-star"></i>' . esc_html_e('Featured', 'listeo_core') . '</div>';
+    $output .= '</div>';
+  }
+
+  return $output;
 }
 add_shortcode('listeo_featured_badge', 'listeo_featured_badge_function');
