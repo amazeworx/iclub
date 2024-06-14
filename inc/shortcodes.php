d3c4e4ad84d7da7981bcba53269dc2b51570bc6c
Slant Dev <build@amazeworx.com>
2024-06-14 21:41:15 +0700

    shortcode

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -190,3 +190,44 @@ function listeo_registration_form_function()
 <?php
 }
 add_shortcode('listeo_registration_form', 'listeo_registration_form_function');
+
+function listeo_login_form()
+{
+  global $post;
+?>
+  <div class="sign-in-form">
+    <form method="post" id="login" class="login" action="<?php echo wp_login_url(); ?>">
+      <?php do_action('listeo_before_login_form'); ?>
+      <p class="form-row form-row-wide">
+        <label for="user_login">
+          <i class="sl sl-icon-user"></i>
+          <input placeholder="<?php esc_attr_e('Username/Email', 'listeo_core'); ?>" type="text" class="input-text" name="log" id="user_login" value="" />
+        </label>
+      </p>
+      <p class="form-row form-row-wide">
+        <label for="user_pass">
+          <i class="sl sl-icon-lock"></i>
+          <input placeholder="<?php esc_attr_e('Password', 'listeo_core'); ?>" class="input-text" type="password" name="pwd" id="user_pass" />
+
+        </label>
+        <span class="lost_password">
+          <a href="<?php echo wp_lostpassword_url(); ?>"><?php esc_html_e('Lost Your Password?', 'listeo_core'); ?></a>
+        </span>
+      </p>
+      <div class="form-row">
+        <?php wp_nonce_field('listeo-ajax-login-nonce', 'login_security'); ?>
+        <input type="submit" class="button border margin-top-5" name="login" value="<?php esc_html_e('Login', 'listeo_core') ?>" />
+        <div class="checkboxes margin-top-10">
+          <input name="rememberme" type="checkbox" id="remember-me" value="forever" />
+          <label for="remember-me"><?php esc_html_e('Remember Me', 'listeo_core'); ?></label>
+
+        </div>
+      </div>
+      <div class="notification error closeable" style="display: none; margin-top: 20px; margin-bottom: 0px;">
+        <p></p>
+      </div>
+    </form>
+  </div>
+<?php
+}
+add_shortcode('listeo_login_form', 'listeo_login_form_function');
